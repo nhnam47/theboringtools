f29244fa610d04ccce474e9199eb01f0e9f4c557
Nguyen Hoang Nam <nhnam47@outlook.com>
2024-12-23 23:24:17 +0700

    push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
+    apiKey: process.env["NG_APP_FIREBASE_API_KEY"],
     authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
     projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
     storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
     messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
     appId: process.env['NG_APP_FIREBASE_APP_ID'],
-    measurementId: process.env['NG_APP_FIREBASE_MEASUREMENT_ID'],
+    measurementId: process.env['NG_APP_FIREBASE_MEASUREMENT_ID']
+  
   },
 };
